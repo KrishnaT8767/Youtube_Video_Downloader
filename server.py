6f9fe95032123beead6df1a68d1202db53da8bad
Krishna Thombre <krishnat.nalanda@gmail.com>
2025-09-30 11:22:04 +0530

    sdf

--- server.py
+++ server.py
@@ -154,5 +154,5 @@ def open_browser():
     webbrowser.open("http://127.0.0.1:5000")
 
 if __name__=="__main__":
-    threading.Timer(1, open_browser).start()
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
